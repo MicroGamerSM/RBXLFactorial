388057833b26df4638d0c9ae50eabe05791e8e29
MicroGamerSM <98235195+MicroGamerSM@users.noreply.github.com>
2022-04-04 12:59:38 -0400

    baseItemCode.lua

---
+++ baseItemCode.lua
@@ -0,0 +1,137 @@
+local data = {}
+--types
+data.types = "90480c26-2a92-465f-b106-f96ef7e11bde-d"
+data.types.debug = "c18c8524-fc69-42f3-b11a-2d53681e3479-d1"
+data.types.surface = "7ed51eea-6f86-4195-8965-73657ed4bda2-d1"
+data.types.underground = "25a0a12e-da55-473d-bb44-860e3cd58f79-d1"
+data.types.special = "d8516049-c340-4091-8dd4-68194b3d82b4-d1"
+data.types.mechanical = "0adeb3da-b17a-4106-a538-afed062f98fb-d1"
+data.types.electrical = "ba2a9c92-03cb-4bdb-aa41-392b04eda951-d1"
+data.types.natural = "2825280a-7153-422e-9b84-a2d24f4dda3a-d1"
+data.types.travel = "916adaed-1df7-4af3-ac09-6b754fbf4464-d1"
+data.types.functional = "a3fcb680-ca2d-4ca9-8cc2-ddafea752dcc-d1"
+data.types.defence = "1c4798fb-f171-4af1-82e9-8337810635ba-d1"
+data.types.offence = "95913ab7-7fe7-46e2-8461-8deb5ff25b92-d1"
+data.types.tools = "c4440c42-6afc-48f6-a81a-9751f762595b-d1"
+data.types.liquid = "72fffb2e-e944-456f-9d96-9b227c10916f-d1"
+--subtypes
+data.subtypes = "86a91f06-91b5-4837-ad96-a57c2c35c3f2-d"
+data.subtypes.liquid = "c7f5d07c-31da-4626-bfe9-d5a402f7a023-d2"
+data.subtypes.defence = "77b3ffe5-011f-402d-9001-702ff1a13f04-d2"
+data.subtypes.offence = "33b069f6-cbe8-4cf4-874c-71f70e70208e-d2"
+data.subtypes.none = "a77e0ac2-9554-4da8-b81b-0177f9f88eeb-d2"
+--tiers
+data.tiers = "ae68e8fb-7493-425f-b04b-84ba19650b7e-d"
+data.tiers.common = "e7fbd2f1-b1ba-4ceb-b117-9289fb5af264-d3"
+data.tiers.uncommon = "e156c391-82e8-4d87-a912-e4b4f377a478-d3"
+data.tiers.semirare = "4b186ec5-7019-4683-9f03-f75e56bbaa4a-d3"
+data.tiers. mostlyrare = "07de8b65-dee8-4cc4-9058-64d7c8c6f640-d3"
+data.tiers.rare = "775888b9-c555-4eaf-882d-2b843e2b97aa-d3"
+data.tiers.semiemril = "ff858ba8-2fd8-4030-b756-5fb8d6ae44bb-d3"
+data.tiers.mostlyemril = "a3a9a2ec-ae39-442d-8c57-5d048f8c45a7-d3"
+data.tiers.emril = "fd9ec33d-3e53-481b-b27a-869841e5c489-d3"
+data.tiers.semilegendary = "e3d36b8f-dc3a-4b70-93f6-e93e318b3ffc-d3"
+data.tiers.mostlylegendary = "af23cfaa-cfa8-449b-b55f-32824d156423-d3"
+data.tiers.legendary = "5d9a12c3-5d7d-4aef-ac66-903fd111614d-d3"
+data.tiers.semimythical = "33c2fb40-3f43-4661-aa2a-bbf4587213cc-d3"
+data.tiers.mostlymythical = "5f203b3f-5655-4819-ab40-3754f20009a7-d3"
+data.tiers.mythical = "148248fb-3c89-4057-a5b4-575cea7f983d-d3"
+--tools
+data.tools = "21fabe86-0940-4244-a36c-921de9a9a7f6-d"
+data.tools.notool = "e8682839-a2a0-4fe3-be95-aa8a52c84244-d4"
+data.tools.notool2 = "d272f9c6-5dbc-40c2-8de0-5513382d5517-d4"
+data.tools.picaxe = "047dd59a-08db-496a-a224-d6ae3abe6f23-d4"
+data.tools.axe = "0b9aac51-07a5-4ee8-bf84-3ea638bc2a00-d4"
+data.tools.shovel = "beb13975-51b3-48c4-bae5-3179b4b0d658-d4"
+data.tools.hoe = "05139dd3-f1ed-48a7-a5ce-beb63bd54b2a-d4"
+data.tools.sword = "35779434-caef-4693-b214-54e02ef0cdff-d4"
+data.tools.bucket = "9e86a739-4845-4635-a9f1-601c53df1af5-d4"
+data.tools.solder = "7b52e566-7410-47e2-b13a-0283882f0101-d4"
+--defence
+data.defence = "9e390feb-fa71-4b93-bec6-07c575d296a9-d"
+data.defence.shield = "356ac09a-d73b-488c-bdf3-f2e81678f1e9-d5"
+data.defence.helmet = "984a7304-6e95-409f-bb0a-450abe557449-d5"
+data.defence.chestplate = "f6560349-5efe-4544-9990-b85fa26f1120-d5"
+data.defence.leggings = "1f023a35-00e4-4afa-8968-ab0c5c367a83-d5"
+data.defence.boots = "057306f9-04e1-4918-9699-645e85cd374f-d5"
+--offence
+data.offence = "08a9d45d-757b-4b9c-a875-320dc703af0a-d"
+data.offence.sword = "7bcccf15-a5f8-40d4-8be4-8261d5abdc07-d6"
+data.offence.bow = "6555fddb-4d0f-4222-a65d-8ef53857f978-d6"
+data.offence.arrow = "7df0f168-6f1c-4f37-a8fb-0a37cdcc7244-d6"
+data.offence.tnt = "1aa409df-52eb-4d8b-b2ee-4666de6e26cf-d6"
+data.offence.bucketlava = "9cc91181-e000-44b6-816b-79aad3f10a2c-d6"
+--tempature
+data.tempature = "ed423f3b-3cd1-4033-8208-2649f78ee056-d"
+data.tempature.verycold = "57ce0801-c114-4d9b-bcaf-8a08d0b24bef-d7"
+data.tempature.almostverycold = "c31d9b00-57ef-4689-995e-048f92957fa4-d7"
+data.tempature.cold = "30d0feb4-efc7-4019-be22-523f49f6f51f-d7"
+data.tempature.almostcold = "8843ce8a-bd2a-411e-896d-77a6fa453ee1-d7"
+data.tempature.chilly = "68a3b9f6-7e41-4e8c-a8dd-b24b43f40b21-d7"
+data.tempature.almostchilly = "7bec6bfb-22a1-45ff-91a1-343908d380c9-d7"
+data.tempature.middle = "ad5e443d-33aa-4be0-bf3d-3d7701680ba8-d7"
+data.tempature.almostwarm = "716a8aad-e9f1-4877-9f4d-e6ee8fb10e4e-d7"
+data.tempature.warm = "6558a4f6-92c0-4278-98bc-69d9d5b3ac23-d7"
+data.tempature.almosthot = "d7ec0fcc-73a3-45f0-8b2b-3330e5fea902-d7"
+data.tempature.hot = "b19db05d-a1bf-435e-ba04-29ab1c9a9024-d7"
+data.tempature.almostveryhot = "24a4b431-c435-48ba-8843-fec932fa1728-d7"
+data.tempature.veryhot = "4585e0f2-96cf-4aad-93ef-f00c842495f4-d7"
+data.tempature.none = "ac656cdf-0256-462a-a4f2-16df94122326-d7"
+--currencies
+data.currencies = "2c219191-b682-40f7-acd0-cc4716a50e60-d"
+data.currencies.coin = "ced21a7d-7db6-48cb-ad8d-5f6124731470-d7"
+data.currencies.coin.value = game
+--sounds
+data.sounds = "c9bf565c-de59-443e-9398-1a2350416c5b-d"
+data.sounds.none = "7f80b04a-4b9b-4444-91ea-61118b7ca7d4-d8"
+
+--stats
+local stats = {}
+
+--basic
+stats.basic = {}
+stats.basic.id = "00000000-0000-0000-0000-000000000000-b0"
+stats.basic.hasDataId = false
+stats.basic.dataId = "00000000-0000-0000-0000-000000000000-d0"
+stats.basic.type = data.types.debug
+stats.basic.subtype = data.subtypes.none
+stats.basic.tier = data.tiers.common
+stats.basic.scripts = game:getService("ReplicatedStorage").scripts
+stats.basic.name = "#DEBUGBLOCK"
+stats.basic.description = "00000000-0000-0000-0000-000000000000-b0"
+--store
+stats.store = {}
+stats.store.buyCost = 0
+stats.store.buyCurrency = data.currencies.coin
+stats.store.sellCost = 0
+stats.store.SellCurrency = data.currencies.coin
+stats.store.iconIsViewport = true
+--liquid
+stats.liquid = {}
+stats.liquid.isLiquid = false
+stats.liquid.spreadSpeed = 0
+stats.liquid.hasSound = false
+stats.liquid.idleSound = data.sounds.none
+stats.liquid.spreadSound = data.sounds.none
+stats.liquid.isRecipePart = false
+stats.liquid.RecipeId = "00000000-0000-0000-0000-000000000000-r0"
+--block
+stats.block = {}
+stats.block.isBlock = true
+stats.canRotateX = false
+stats.canRotateY = false
+stats.canRotateZ = false
+--texture
+stats.texture = {}
+stats.texture.xPositive = "00000000-0000-0000-0000-000000000000-t0"
+stats.texture.xNegative = "00000000-0000-0000-0000-000000000000-t0"
+stats.texture.yPositive = "00000000-0000-0000-0000-000000000000-t0"
+stats.texture.yNegative = "00000000-0000-0000-0000-000000000000-t0"
+stats.texture.zPositive = "00000000-0000-0000-0000-000000000000-t0"
+stats.texture.zNegative = "00000000-0000-0000-0000-000000000000-t0"
+
+--Misc
+stats.creators = "MicroGamer @Tommy2Lips"
+
+
+return stats
